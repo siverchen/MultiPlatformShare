65cddfb95615d43bedbc3fbb3e2cee8ba07d56ff
siverchen <siverchen@gmail.com>
2013-03-11 10:29:20 +0800

    init

---
+++ MultiPlatformShare/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MultiPlatformShare
+//
+//  Created by Chen Lei on 13-3-11.
+//  Copyright (c) 2013年 cmc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MultiPlatformShare/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MultiPlatformShare
+//
+//  Created by Chen Lei on 13-3-11.
+//  Copyright (c) 2013年 cmc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
